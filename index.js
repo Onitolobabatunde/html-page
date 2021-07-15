3ca1cef1a7317984dacb8cef2acec279ba3baf76
onitololaurence@gmail.com <56078333+Onitolobabatunde@users.noreply.github.com>
2021-07-15 22:42:17 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,398 @@
+// document.getElementById("count-el").innerText = 10
+
+// let firstBatch = 5
+// let secondBatch = 7
+
+// let count = firstBatch + secondBatch
+
+// console.log(count)
+// count = 5 * 7
+// console.log(count)
+
+
+// // let firstBatch = 4
+// // let secondBatch = 7
+
+// // let count = firstBatch + secondBatch
+// // console.log(count)
+
+// // let count = 5
+
+// // count = count + 10
+
+// // count = count - 2
+// // console.log(count)
+
+// // let myAge = 35
+// // let humanDogRatio = 7
+
+// // let myDogAge = myAge * humanDogRatio
+
+// // let bonusPoints = 50
+// // console.log(bonusPoints)
+
+// // bonusPoints = bonusPoints + 50
+// // console.log(bonusPoints)
+
+// // bonusPoints = bonusPoints - 75
+// // console.log(bonusPoints)
+
+// // bonusPoints = bonusPoints + 45
+// // console.log(bonusPoints)
+
+// // bonusPoints = bonusPoints + 35
+// // console.log(bonusPoints)
+
+
+// // console.log(myAge)
+
+// // let a = 0
+// // a = 1
+
+// // const three = 1 + 2
+// // const four = three + 1
+
+// // let a = 5
+// // let b = 4
+
+// // console.log(a + b)
+
+// // let a = 1 * 2 + 5 / 2 % 2
+
+// // function getData(color = 'black', age = 25) {
+// // }
+
+// // function getData() {
+// //     return ['Flavio', 37]
+// // }
+
+// // let [name, age] = getData()
+
+// // const getData = () => {
+// //     const dosomething = () => {}
+// //     dosomething()
+// //     return 'test'
+// // }
+
+// // () => {
+
+// // const car = new Object()
+
+// // 
+
+// // function getData() {
+// //     return ['Flavio', 37]
+// // }
+
+// // let [name, age] = getData()
+// // let a = 20
+// // let c = 60
+// // console.log(a + c)
+// // let age = 36
+// // let myAge = age
+// // myAge = 37
+// // age
+// // let age = 2
+// // console.log(age)
+
+// // function getData() {
+// //     var data
+// //     if (ture) {
+// //         data = 'some data'
+// //     }
+// //     console.log(data)
+// // }
+
+// // var a = 3
+// //     // var b = 6
+// // console.log(a)
+// function doLoop() {
+//     for (var i = 5; i < 20; i++) {
+//         console.log(i)
+//     }
+//     console.log(i)
+// }
+// doLoop()
+
+// let myAge = 35
+// let humanDogRatio = 7
+
+// let myDogAge = myAge * humanDogRatio
+
+// console.log(myDogAge)
+
+// let count = 5
+// count = count + 1
+// console.log(count)
+// function increment() {
+//     console.log("The button was clicked")
+// }
+
+// function countdown() {
+//     console.log(5)
+//     console.log(4)
+//     console.log(3)
+//     console.log(2)
+//     console.log(1)
+// }
+
+// countdown()
+
+// // console.log(5)
+// // console.log(4)
+// // console.log(3)
+// // console.log(2)
+// // console.log(1)
+
+// countdown()
+
+// function myLogger() {
+//     console.log(42)
+// }
+
+// myLogger()
+
+// let lap1 = 34
+// let lap2 = 33
+// let lap3 = 36
+
+// function logLapTime() {
+//     let totalTime = lap1 + lap2 + lap3
+//     console.log(totalTime)
+// }
+
+// logLapTime()
+
+// let lapsCompleted = 0
+
+// function incrementLap() {
+//     lapsCompleted = lapsCompleted + 1
+// }
+
+// incrementLap()
+// incrementLap()
+// incrementLap()
+
+// console.log(lapsCompleted)
+
+
+// let count = 0
+
+// function increment() {
+//     console.log("clicked")
+//     count = count + 1
+//     console.log(count)
+// }
+
+// let countEL = document.getElementById("count-el")
+// console.log(countEL)
+
+// let count = 0
+
+// function increment() {
+//     count = count + 1
+//     countEL.innerText = count
+// }
+
+// Save Button
+
+// let countEL = document.getElementById("count-el")
+// let count = 0
+
+// function increment() {
+//     count = count + 1
+//     countEL.innerText = count
+// }
+
+// function save() {
+//     console.log(count)
+// }
+
+
+// What is a String
+
+// let username = "per"
+// let message = "You have tree new notifications"
+
+// let messageToUser = message + ", " + username + "!"
+// console.log(messageToUser)
+
+// Log a greeting to the console
+
+// let name = "Temitope"
+// let greeting = "Hi, my name is"
+
+// let myGreeting = greeting + " " + name
+// console.log(myGreeting)
+
+// Lesson 21: Strings vs. Numbers
+
+// let name = 42
+// let greeting = "Hi, my name is "
+// let myGreeting = greeting + name
+// console.log(myGreeting)
+
+// let points = 4
+// let bonusPoints = "10"
+
+// let totalPoints = points + bonusPoints
+
+// console.log(totalPoints)
+
+// console.log(4 + 5)
+// console.log("2" + "4")
+// console.log("5" + 1)
+// console.log(100 + "100")
+
+
+let name = "Per Harald Borgen"
+let greeting = "Welcome back"
+    // Is showing an error while debugging it. 
+
+let welcomeEl = document.getElementById("welcome-el")
+
+welcomeEL.innerText = greeting + name
+
+welcomeEL.innerText += "!"
+
+//Use plus equal for count
+// let countEl = document.getElementById("count-el")
+// let count = 0
+
+// function increment() {
+//     count = count + 1
+//     countEl.textContent = count
+// }
+
+// function save() {
+//     console.log(count)
+// }
+
+
+
+
+// let firstName = "Per"
+// let lastName = "Borgen"
+
+// let fullName = firstName + lastName
+
+// console.log(fullName)
+
+// let name = "Linda"
+// let greeting = "Hi there"
+
+// function greetLinda() {
+//     console.log(greeting + ", " + name + "!")
+// }
+
+// greetLinda()
+
+
+//Incrementing and Decrementing
+
+// let myPoints = 3
+
+// function add3Points() {
+//     myPoints += 3
+// }
+
+// function removalPoints() {
+//     myPoints -= 1
+// }
+
+// add3Points()
+// add3Points()
+// add3Points()
+// removalPoints()
+// removalPoints()
+
+
+
+// console.log(myPoints)
+
+//String and Number
+// console.log("2" + 2) //"22"
+// console.log(11 + 7) //18
+// console.log(6 + "5") // "65"
+// console.log("My points: " + 5 + 9) //"My points: 59"
+// console.log(2 + 2) //4
+// console.log("11" + "14") //1114
+
+//Rendering an error message
+
+// let errorParagraph = document.getElementById("error")
+// console.log(errorParagraph)
+
+// function purchase() {
+//     console.log("button clicked")
+//     errorParagraph.textContent = "Something went wrong, please try again later"
+// }
+
+// let num1 = 8
+// let num2 = 2
+// document.getElementById("num1-el").textContent = num1
+// document.getElementById("num2-el").textContent = num2
+
+
+// let sumEL = document.getElementById("sum-el")
+
+// function add() {
+//     let result = num1 + num2
+//     sumEL.textContent = "Sum: " + result
+// }
+
+// function substract() {
+//     let result = num1 - num2
+//     sumEL.textContent = "Sum: " + result
+// }
+
+// function substract() {
+//     let result = num1 - num2
+//     sumEL.textContent = "Sum: " + result
+// }
+
+// function multiply() {
+//     let result = num1 * num2
+//     sumEL.textContent = "Sum: " + result
+// }
+
+// function divide() {
+//     let result = num1 / num2
+//     sumEL.textContent = "Sum: " + result
+// }
+
+
+// Let's build a Blackjack game
+// Add the firstCard, secondCard, and sum
+
+// let firstCard = 6
+// let secondCard = 9
+
+// let sum = firstCard + secondCard
+
+// console.log(sum)
+
+
+//If...else conditionals
+
+//Add the isAlive variable
+// let firstCard = 10
+// let secondCard = 11
+// let sum = firstCard + secondCard
+// let hasBlackJack = false
+// let isAlive = true
+
+// if (sum < 20) {
+//     console.log("Do you want to draw a new card?")
+// } else if (sum === 21) {
+//     console.log("Wohoo! You've got Blackjack! ")
+//     hasBlackJack = true
+
+// } else {
+//     console.log("You're out of the game! ")
+//     isAlive = false
+// }
+
+// console.log(hasBlackJack)
+
+// LET'S PRACTICE BOOLEAN CONDITIONS
